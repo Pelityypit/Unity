44fb828913c6aa8f9262d16ad83f7e75127b1ab3
Janita <bgj369@myy.haaga-helia.fi>
2021-02-10 18:16:43 +0200

    Päävalikko tehty, play-nappi aloittaa nyt pelin

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadGame() 
+   {
+       // Loads to next scene
+       //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GameScene");
+       
+   }
+
+   
+
+   public void QuitGame()
+   {
+       Application.Quit();
+       Debug.Log("Quit!");
+   }
+}
