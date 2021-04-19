e7c4c4b959b542754ddc7c1140f7c36806d073be
joona127 <65039165+joona127@users.noreply.github.com>
2021-04-19 14:30:47 +0300

    tietokanta alustettu

---
+++ Assets/Scenes/Scoreboard/HighScoreTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake() {
+        entryContainer = transform.Find("highscorecontainer");
+        entryTemplate = entryContainer.Find("highscoretemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 20f;
+        for (int i = 0; i < 10; i++) {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();  
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i); 
+            entryRectTransform.gameObject.SetActive(true);
+             }
+
+    }
+}
---
+++ Assets/Scripts/SimpleDB.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+
+
+
+public class SimpleDB : MonoBehaviour
+{
+    private string dbName = "URI=file:Database.db";
+    // Start is called before the first frame update
+    void Start()
+    {
+        // run the method to create the table
+        CreateDB();
+
+        DisplayQuestions();
+        
+    }
+
+    public void CreateDB()
+    {
+        // Create the db connection
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            // set up and object (called "command") to allow db control
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE IF NOT EXISTS questions (text VARCHAR(50);";
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+        }
+    }
+
+    public void addQuestion() {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO questions (text) VALUES ('" + "Mitä kuuluu?" + "');";
+                command.ExecuteNonQuery(); //  this runs the SQL command
+            }
+            connection.Close();
+        }
+    }
+
+    public void DisplayQuestions()
+    {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                // select everything from the table "questions"
+                command.CommandText = "SELECT * FROM weapons;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    Debug.Log("Question: " + reader["text"]);
+
+                    reader.Close();
+                }
+            }
+            connection.Close();
+        }
+    }
+
+    
+}
